ad0a40b630cba6545a7b28bc48b4317eefb94cc9
Mariane7702 <mariane.vigna@efrei.net>
2024-03-05 17:06:30 +0100

    data base connection and docker creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import { join } from 'path';
-import { IncidentModule } from './incident/incident.module';
+import {ConfigModule} from "@nestjs/config";
+import {DatabaseModule} from "./shared/database/database.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject : [ConfigService],
-      useFactory: (configService: ConfigService)=> ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-        synchronize: true,
-      })
-    }),
-    IncidentModule,
+      DatabaseModule,
+      ConfigModule.forRoot(),
+
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
